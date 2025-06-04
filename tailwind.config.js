890ee2141bfffa1fe889653140e2e98fb5915b78
R1Jake <jakerubinstein11@gmail.com>
2025-06-04 10:05:21 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
   ],
   theme: {
     extend: {
       colors: {
-        background: "#0f0f0f",
-        primary: "#a855f7",
-      },
-      fontFamily: {
-        sans: ["'Space Grotesk'", "sans-serif"]
+        background: "#0a0a0a",
+        primary: "#8e44ad"
       }
     },
   },
   plugins: [],
-};
+}
